41005bfa9e299a16994faf112e1f5881f1cbca94
MarLeo <martin.aurele12@gmail.com>
2017-04-18 00:32:51 +0200

    cleaning driver class

--- src/main/java/driver/Driver.java
+++ src/main/java/driver/Driver.java
@@ -4,16 +4,14 @@ import dao.CoursDao;
 import dao.FormationDao;
 import dao.GenericDao;
 import dao.SalleDao;
-import domain.*;
+import domain.Cours;
+import domain.Formation;
+import domain.Salle;
+import domain.SalleId;
 import exceptions.BusinessException;
-import org.hibernate.HibernateException;
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import util.HibernateUtil;
-
-import java.util.Iterator;
-import java.util.List;
 
 /**
  * Created by marti on 16/04/2017.
@@ -26,30 +24,6 @@ public class Driver {
     private static Transaction transaction = null;
     public static void main(String[] args) throws BusinessException {
 
-
-       //  Driver driver = new Driver();
-
-        /* Add few address records in database*/
-      //  Address address1 = driver.addAddress("13 Avenue Jean Jaurés", "Bagneux", "Paris", 92220);
-      //  Address address2 = driver.addAddress("Corso Francia", "Torino", "Piemonte", 74440);
-
-        /* Add few employee records in database */
-      //  Integer empID1 = driver.addEmployee("Zara", "Ali", 1000, address1); // address1 will be add because of cascade type = ALL
-      //  Integer empID2 = driver.addEmployee("Daisy", "Das", 5000, address2);
-      //  Integer empID3 = driver.addEmployee("John", "Paul", 10000, address1);
-
-        /* List down all the employees */
-      //  driver.listEmployees();
-
-        /* Update employee's records */
-      //  driver.updateEmployee(empID1, 5000);
-
-        /* Delete an employee from database */
-      //  driver.deleteEmployee(empID3);
-
-        /* List dow new list of the employees */
-      //  driver.listEmployees();
-      //  Transaction transaction = null;
         GenericDao<Formation, Integer> formationDao = new FormationDao();
         GenericDao<Cours, Integer> coursDao = new CoursDao();
         GenericDao<Salle, SalleId> salleDao = new SalleDao();
@@ -58,26 +32,6 @@ public class Driver {
         Formation finance = new Formation("Master 2 Finance");
         Formation sitn = new Formation("Master 2 SITN");
         Formation id = new Formation("Master 2 ID");
-
-        // some transient courses object
-
-        /*Cours J2EE = new Cours("J2EE");
-        Cours bigData = new Cours("BigData");
-
-        coursDao.create(sessionFactory, transaction, optimization);
-        coursDao.create(sessionFactory, transaction, J2EE);
-        coursDao.create(sessionFactory, transaction, bigData);
-        */
-
-        // assign a cours to a formation
-        /*finance.addCours(optimization);
-        finance.addCours(J2EE);
-        finance.addCours(bigData);
-
-        sitn.addCours(J2EE);
-        id.addCours(bigData);
-        */
-
 
 
         // load some formation to database
@@ -98,7 +52,6 @@ public class Driver {
         formation_finance.addCours(cours_optimization);
         formation_finance.addCours(cours_J2EE);
         formation_sitn.addCours(coursDao.create(sessionFactory, transaction, bigdata));
-        //formationDao.update(sessionFactory, transaction, formation_finance);
 
         // list all formations from database
         System.out.println("List of formations: ");
@@ -179,109 +132,8 @@ public class Driver {
         salleDao.findAll(sessionFactory, transaction);
 
 
-
-
         sessionFactory.close();
     }
-
-
-    public Address addAddress(String street, String city, String state, Integer zipcode) {
-        Transaction transaction = null;
-        Integer addressID = null;
-        Address address = null;
-        try (Session session = sessionFactory.openSession()) {
-            transaction = session.beginTransaction();
-            address = new Address(street, city, state, zipcode);
-            session.save(address);
-            transaction.commit();
-        } catch (HibernateException he) {
-            if (transaction != null) transaction.rollback();
-            he.printStackTrace();
-        }
-        return address;
-    }
-
-    public Integer addEmployee(String firstname, String lastname, int salary, Address address) {
-        Transaction transaction = null;
-        Integer employeeID = null;
-
-        try (Session session = sessionFactory.openSession()) {
-            transaction = session.beginTransaction();
-            Employee employee = new Employee(firstname, lastname, salary, address);
-            employeeID = (Integer) session.save(employee);
-            transaction.commit();
-        } catch (HibernateException he) {
-            if (transaction != null) transaction.rollback();
-            he.printStackTrace();
-        }
-        return employeeID;
-    }
-
-
-    public void listEmployees() {
-        Transaction transaction = null;
-
-        try (Session session = sessionFactory.openSession()) {
-            transaction = session.beginTransaction();
-            List employees = session.createQuery("FROM Employee").list();
-            for (Iterator iterator = employees.iterator(); iterator.hasNext(); ) {
-                Employee employee = (Employee) iterator.next();
-                System.out.println("First Name: " + employee.getFirstname());
-                System.out.println("Last Name: " + employee.getLastname());
-                System.out.println("Salary: " + employee.getSalary());
-                Address address = employee.getAddress();
-                System.out.println("Address ");
-                System.out.println("\tStreet: " + address.getStreet_name());
-                System.out.println("\tCity: " + address.getCity_name());
-                System.out.println("\tState: " + address.getState_name());
-                System.out.println("\tZipcode: " + address.getZipcode());
-            }
-            transaction.commit();
-        } catch (HibernateException he) {
-            if (transaction != null) transaction.rollback();
-            he.printStackTrace();
-        }
-    }
-
-
-    public void updateEmployee(Integer employeeID, int salary) {
-        Transaction transaction = null;
-
-        try (Session session = sessionFactory.openSession()) {
-            transaction = session.beginTransaction();
-            Employee employee = session.get(Employee.class, employeeID);
-            employee.setSalary(salary);
-            session.update(employee);
-            transaction.commit();
-        } catch (HibernateException he) {
-            if (transaction != null) transaction.rollback();
-            he.printStackTrace();
-        }
-
-    }
-
-
-    public void deleteEmployee(Integer employeeID) {
-        Transaction transaction = null;
-
-        try (Session session = sessionFactory.openSession()) {
-            transaction = session.beginTransaction();
-            Employee employee = session.get(Employee.class, employeeID);
-            session.delete(employee);
-            transaction.commit();
-        } catch (HibernateException he) {
-            if (transaction != null) transaction.rollback();
-            he.printStackTrace();
-        }
-
-    }
-
-
-
-
-
-
-
 
 
 }
